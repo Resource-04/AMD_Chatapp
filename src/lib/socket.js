43859e878e0db4e81010c865da475731abc19545
Resource-04 <resource4.code4bharat@gmail.com>
2025-05-06 14:30:47 +0530

    feat: Update CORS configuration to allow additional origins

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://www.shourk.com", "https://shourk.com"],
     methods: ["GET", "POST" , "PUT" , "DELETE"],
     credentials: true
   },
